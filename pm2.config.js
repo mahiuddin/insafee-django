0e3dcf609a069c7d32f7051efcd6ff439f52d10c
Mahi Uddin <mahi.ewu@gmail.com>
2025-01-01 22:33:54 +0600

    bootstrap install

--- pm2.config.js
+++ pm2.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     apps: [
     {
-    name: “Insafee”,
-    script: “/var/www/insafee.com/venv/bin/gunicorn”,
-    args: “insafee.wsgi:application — workers 3 — bind *******:8004”,
-    cwd: “/var/www/insafee.com/insafee”,
-    interpreter: “/var/www/insafee.com/venv/bin/python3.12”,
+    name: "Insafee",
+    script: "/var/www/insafee.com/venv/bin/gunicorn",
+    args: "insafee.wsgi:application --workers 3 --bind 0.0.0.0:8004",
+    cwd: "/var/www/insafee.com/insafee",
+    interpreter: "/var/www/insafee.com/venv/bin/python3.12",
     env: {
-        DJANGO_SETTINGS_MODULE: “insafee.settings”,
+        DJANGO_SETTINGS_MODULE: "insafee.settings",
      },
     },
  ],
